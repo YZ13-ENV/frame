87f229a8703c321e1325dac9afddce35137cb355
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-01-30 12:50:37 +0500

    remove grid from shots layout

--- app/(shots)/(routes)/shots/layout.tsx
+++ app/(shots)/(routes)/shots/layout.tsx
@@ -1,11 +1,9 @@
-import { file } from "@/api/file"
 import { Suspense } from "react"
-import FrameMark from "@/components/shared/frame-mark"
-import Image from "next/image"
 import dynamic from "next/dynamic"
 import HeaderSkeleton from "@/components/skeletons/header"
 import Nav from "../../_components/nav"
 import Footer from "@/components/shared/footer"
+import { StarField } from "ui"
 const Header = dynamic(() => import( "@/components/widgets/header"), {
     loading: () => <HeaderSkeleton />
 })
@@ -14,20 +12,12 @@ type Props = {
     children: JSX.Element | JSX.Element[]
 }
 const layout = async({ children }: Props) => {
-    const grid = await file.static.get('gird.svg')
     return (
         <>
             <Suspense fallback={<HeaderSkeleton />}>
                 <Header />
             </Suspense>
             <div className="min-h-screen w-full relative py-12 lg:px-24 md:px-12 px-6">
-                {
-                    grid &&
-                    <>
-                        <div className='w-full h-full z-[-1] max-h-screen absolute -top-[60px] left-0 bg-gradient-to-b from-transparent to-background' />
-                        <Image src={grid} fill className='z-[-2] max-h-screen absolute !-top-[60px] left-0 object-cover opacity-40' alt='grid' />
-                    </>
-                }
                 <Nav />
                 <div className="w-full h-full z-20 grid shots_grid gap-6">
                     { children }
