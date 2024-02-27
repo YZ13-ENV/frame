4a4fd3c8f86b2a539537de28b42f5a69968d50d5
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-02-27 19:25:32 +0500

    fix user layout

--- app/(user)/_components/banner/author-stats.tsx
+++ app/(user)/_components/banner/author-stats.tsx
@@ -1,19 +1,19 @@
 import { PortfolioConfig } from '@/helpers/getPortfolio'
-import { team, bum } from 'api'
+import { bum, team } from 'api'
 
 type Props = {
     type: PortfolioConfig['type']
     id: string
 }
-const AuthorStats = async({ id, type }: Props) => {
+const AuthorStats = async ({ id, type }: Props) => {
     const likesPromise = type === 'team' ? team.likes(id) : bum.author.likes(id)
     const followersPromise = bum.author.followers(id)
     const followingsPromise = type === 'team' ? [] : bum.author.followings(id)
     const [likes, followings, followers] = await Promise.all([likesPromise, followingsPromise, followersPromise])
     return (
-        <div className="flex items-center gap-2 w-fit h-fit">
+        <div className="flex items-center gap-2 w-fit h-fit mt-6">
             <span className="text-sm text-muted-foreground">{followers.length} Подписчиков</span>
-            { type === 'user' && <span className="text-sm text-muted-foreground">{followings.length} Подписок</span> }
+            {type === 'user' && <span className="text-sm text-muted-foreground">{followings.length} Подписок</span>}
             <span className="text-sm text-muted-foreground">{likes.length} Лайков</span>
         </div>
     )
