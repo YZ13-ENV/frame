c5fe5358b82368e4f02301019636f36aa8d5448a
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-03-07 09:50:12 +0500

    fix portfolio page

--- app/(user)/(routes)/[nick]/page.tsx
+++ app/(user)/(routes)/[nick]/page.tsx
@@ -22,6 +22,7 @@ const page = async ({ params }: Props) => {
     const { nick } = params
     const portfolio = await getPortfolio(nick)
     const teamId = portfolio.type === 'team' && portfolio.data ? portfolio.data.doc_id : null
+
     if (portfolio.type === 'user' && portfolio.data && portfolio.data.nickname && portfolio.data.nickname !== nick)
         return redirect(`/${portfolio.data.nickname}`)
     if (portfolio.type === 'team') return (
@@ -37,7 +38,7 @@ const page = async ({ params }: Props) => {
     return (
         <div className="w-full min-h-[17rem] px-6 max-w-screen-2xl mx-auto my-24">
             {
-                portfolio && portfolio.data && portfolio.data.uid &&
+                portfolio && portfolio.data &&
                 <div className="z-20 grid w-full h-full gap-6 shots_grid">
                     <AdvancedChunk getter={bum.shots.byUser(portfolio.data.uid, 'new')} />
                 </div>
