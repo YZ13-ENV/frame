c2a301dd13fdf4a51f4cfd83695aa86c0fcabe91
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-03-24 10:37:09 +0500

    update /uploads layout

--- components/shared/user-circle.tsx
+++ components/shared/user-circle.tsx
@@ -50,7 +50,7 @@ const User = ({ size = 36 }: Props) => {
     }, [user])
     return (
         <>
-            {session && !session.activeUid && <OneClickAuth members={session.members} user={user} onUser={uid => controls('update', uid)} />}
+            {session && !session.activeUid && !!session.members.length && <OneClickAuth members={session.members} user={user} onUser={uid => controls('update', uid)} />}
             <UserCircle size={size} isSubscriber={isSubscriber} map={menuWithSignOut} loginLink={link}
                 activeMenu={isTabletOrMobile ? 'mobile' : 'desktop'} user={user as User | undefined} buttonSize='lg' />
         </>
